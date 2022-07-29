cacba66e5b8643248dc6580c5e2b186b041a1c1d
mchor01 <81789617+mchor01@users.noreply.github.com>
2022-07-28 21:20:49 -0700

    practice

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,11 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+        $("html").css("background-color", "#2B7129")
+        $("#wear").attr("src","images/spring-wear.jpg")
+        $("#logo").attr("src","images/spring.gif")
+        $("header h3").text("Jump into Spring!")
+
     });
 });
